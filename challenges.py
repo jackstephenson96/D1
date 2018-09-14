8b33c9499d935916cc4308fda2276c386ea95347
Jack <jackstephenson96@gmail.com>
2018-09-14 13:49:56 -0400

    finished discussion 1

--- challenges.py
+++ challenges.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 #Task 2 : Dynamic URLS 
     #edit the view function to display 'Welcome to <course_name>' on localhost:5000/course/<course>
 @app.route('/course/<course_name>')
-def courseView():
-    return ""
-
+def courseView(course_name):
+    return "Welcome to " + course_name
 #Task 3.1 Basic HTML Form
     #Set the method and action of the HTML form, such that form data is sent to /result using POST method
     #The form should have a text field in which you can enter an ingredient (milk, eggs, etc)
@@ -19,7 +18,11 @@ def formView():
     html_form = '''
     <html>
     <body>
-    <form>
+    <form action="/result" method = "POST"> 
+        <label for ="ingredient"> Enter an ingredient : </label>
+        <br>
+        <input type="text" name="ingredient" id="ingredient"></input>
+        <input type="submit" name="Submit"></input>
     </form>
     </body>
     </html>
@@ -31,11 +34,29 @@ def formView():
 def resultView():
     # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results 
     #Step 1 : Receive the ingredient from the form if request type is POST
+
+    if request.method == "POST":
+        ingredient = str(request.form.get("ingredient"))
+
     #Step 2 : Create paramaters JSON with the ingredient received in step 1 in the form required by http://www.recipepuppy.com/about/api/
+
+        params = {}
+        params["i"] = ingredient
+
     #Step 3 : Make an API request to Recipe API and parameters in Step 2 
+
+        baseurl = "http://www.recipepuppy.com/api/"
+        r = requests.get(baseurl, params=params)
+       
     #Step 4 : Parse the response from API request in JSON 
+
+    response_json = json.loads(r.text)
+
     #Step 5 : Display the response in browser (remember : HTML takes only strings)
-    return ""
+
+    response_str = str(response_json)
+
+    return response_str
 
 
 if __name__ == '__main__':
